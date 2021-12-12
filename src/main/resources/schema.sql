ac85d453249bdce5f532ad619379755fa7e5a7f7
Ivan Fomkin <ivan-fomkin@yandex.ru>
2021-12-12 23:37:40 +0300

    Миграция с H2 на PostgreSQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,7 @@
 DROP TABLE IF EXISTS books;
-
 CREATE TABLE books
 (
-    id        INT AUTO_INCREMENT PRIMARY KEY,
+    id        BIGSERIAL PRIMARY KEY,
     author_id VARCHAR(250) NOT NULL,
     title     VARCHAR(250) NOT NULL,
     price_old VARCHAR(250) DEFAULT NULL,
@@ -12,7 +11,7 @@ CREATE TABLE books
 DROP TABLE IF EXISTS authors;
 CREATE TABLE authors
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     first_name varchar(250) NOT NULL,
     last_name  varchar(250) NOT NULL
 );
