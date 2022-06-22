f0593561bb80922f3de6014dc475a76666152f2e
Ivan Fomkin <ivan-fomkin@yandex.ru>
2022-06-22 15:53:53 +0300

    Добавил тесты для BookRepository

--- src/test/java/com/example/MyBookShopApp/AbstractTest.java
+++ src/test/java/com/example/MyBookShopApp/AbstractTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest(classes = TestContainersConfig.class)
 @AutoConfigureMockMvc
 public abstract class AbstractTest {
